c918adca6bb40acd0030b16329a882fa9f5cf22a
yang7206 <y7206062@gmail.com>
2015-10-08 13:30:36 +0800

    Update Android.mk

--- JniLib/jni/Android.mk
+++ JniLib/jni/Android.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 #
 LOCAL_PATH := $(call my-dir)
-
+#构建一个静态库，bspatch中需要调用libbz.a和bzlib.h中的函数或参数，需要先构建
 #building static library
 include $(CLEAR_VARS)
 LOCAL_MODULE:= libbz
@@ -22,20 +22,27 @@ LOCAL_EXPORT_C_INCLUDES := bzlib.h
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
+#
+#构建so文件
 # build so lib
+#so文件名，构建完成会自动将libbspatch.so放在libs\armeabi文件夹之下
 LOCAL_MODULE:= libbspatch  
 # All of the source files that we will compile.  
+#编译文件夹
 LOCAL_SRC_FILES:= \
 	bspatch.c \
 	bsdiff.c
-# Also need the JNI headers.  
+# Also need the JNI headers.
+#导入bzlib文件
 LOCAL_EXPORT_C_INCLUDES := bzlib.h
-# No static libraries.  
+# No static libraries.
+#导入上面构建静态库
 LOCAL_STATIC_LIBRARIES := \
 	libbz  
 LOCAL_LDLIBS += -L$(SYSROOT)/usr/lib -llog
 # No special compiler flags.  
 LOCAL_CFLAGS +=  
- 
- #build so file
+
+#构建共享库
+#build so file
 include $(BUILD_SHARED_LIBRARY)  
